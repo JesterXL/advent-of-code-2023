42506737266bb6aacb90af5591f7b75887eec24d
Jesse Warden <jesterxl@jessewarden.com>
2023-11-23 22:02:16 -0500

    part 2

--- src/Main.elm
+++ src/Main.elm
@@ -1,55 +1,67 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text, ul, li, a, b)
-import Html.Events exposing (onClick)
-import Html.Attributes exposing (class, href)
-import Url
-import Url.Parser exposing ((</>), s, parse, map, oneOf)
 import Browser.Navigation as Nav
+import Html exposing (Html, a, b, button, div, li, text, ul)
+import Html.Attributes exposing (class, href)
+import Html.Events exposing (onClick)
+import Url
+import Url.Parser exposing ((</>), map, oneOf, parse, s)
+
+
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
     ( initialModel key url (parseRoute url)
     , Cmd.none
     )
 
+
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
     , route : Route
-    , floor : Int }
+    , floor : Int
+    , basementCharacter : Int
+    }
+
 
 initialModel : Nav.Key -> Url.Url -> Route -> Model
 initialModel key url route =
     { key = key
     , url = url
     , route = route
-    , floor = 0 }
+    , floor = 0
+    , basementCharacter = 0
+    }
 
 
-type Route 
+type Route
     = NotFound
     | Day12015Warmup
     | Day1
     | Day2
 
+
 routeParser : Url.Parser.Parser (Route -> a) a
 routeParser =
-    oneOf [
-        map Day12015Warmup (s "warmup")
+    oneOf
+        [ map Day12015Warmup (s "warmup")
         , map Day1 (s "day1")
         , map Day2 (s "day2")
-    ]
-    
+        ]
+
+
 parseRoute : Url.Url -> Route
 parseRoute url =
     parse routeParser url
         |> Maybe.withDefault NotFound
 
+
 type Msg
     = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
     | ParseFloors
+    | ParseBasementCharacterPosition
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -67,8 +79,10 @@ update msg model =
 
                 Browser.External href ->
                     ( model, Nav.load href )
+
         UrlChanged url ->
             ( { model | url = url, route = parseRoute url }, Cmd.none )
+
         ParseFloors ->
             let
                 floor =
@@ -78,61 +92,108 @@ update msg model =
                     -- "(()(()(" -- 3
                     -- 232, 1st try, OH YEAH
                     largeInput2015Day1
-                    |> String.split ""
-                    |> List.map (\char ->
-                        if char == "(" then
-                            1
-                        else
-                            -1
-                        )
-                    |> List.sum
+                        |> String.split ""
+                        |> List.map
+                            (\char ->
+                                if char == "(" then
+                                    1
+
+                                else
+                                    -1
+                            )
+                        |> List.sum
             in
             ( { model | floor = floor }, Cmd.none )
 
+        ParseBasementCharacterPosition ->
+            let
+                basementCharacter =
+                    -- ")" -- 1
+                    -- "()())" -- 5
+                    -- 7000 -- too high
+                    -- 1783 -- just right!
+                    largeInput2015Day1
+                        |> String.split ""
+                        |> List.map
+                            (\char ->
+                                if char == "(" then
+                                    1
 
-    
+                                else
+                                    -1
+                            )
+                        |> List.foldl
+                            (\increment acc ->
+                                if acc.basementYet == False && acc.floor + increment == -1 then
+                                    { acc | floor = acc.floor + increment, position = acc.position + 1, basementYet = True }
+
+                                else if acc.basementYet == False then
+                                    { acc | floor = acc.floor + increment, position = acc.position + 1 }
+
+                                else
+                                    acc
+                            )
+                            { floor = 0, position = 0, basementYet = False }
+                        |> .position
+            in
+            ( { model | basementCharacter = basementCharacter }, Cmd.none )
+
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "Advent of Code 2023"
-    , body = 
-        [ div [ class "w-full bg-gray-800 border-gray-200"] [ 
-            tabs
+    , body =
+        [ div [ class "w-full bg-gray-800 border-gray-200" ]
+            [ tabs
             , case model.route of
                 NotFound ->
                     div [] [ text "Not found" ]
+
                 Day12015Warmup ->
-                    div [][ 
-                        text "Day 1 2015 Warmup"
-                        , button 
+                    div []
+                        [ text "Day 1 2015 Warmup"
+                        , button
                             [ class "ext-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                            , onClick ParseFloors]
-                            [text "Calculate Floor"]
-                        , text ("Floor: " ++ (String.fromInt model.floor))]
+                            , onClick ParseFloors
+                            ]
+                            [ text "Calculate Floor" ]
+                        , div [] [ text ("Floor: " ++ String.fromInt model.floor) ]
+                        , button
+                            [ class "ext-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                            , onClick ParseBasementCharacterPosition
+                            ]
+                            [ text "Calculate Basement Character Position" ]
+                        , div [] [ text ("Basement Position: " ++ String.fromInt model.basementCharacter) ]
+                        ]
+
                 Day1 ->
                     div [] [ text "Day 1" ]
+
                 Day2 ->
-                    div [] [ text "Day 2" ] 
+                    div [] [ text "Day 2" ]
             ]
         ]
     }
 
+
 tabs =
-    ul [ class "flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"][
-        li [ class "me-2"][
-            a [ class "inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500", href "/warmup" ][ text "Day 1 2015 Warmup"]
+    ul [ class "flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" ]
+        [ li [ class "me-2" ]
+            [ a [ class "inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500", href "/warmup" ] [ text "Day 1 2015 Warmup" ]
+            ]
+        , li [ class "me-2" ]
+            [ a [ class "inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500", href "/day1" ] [ text "Day 1" ]
+            ]
+        , li [ class "me-2" ]
+            [ a [ class "inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300", href "/day2" ] [ text "Day 2" ]
+            ]
         ]
-        , li [ class "me-2"][
-            a [ class "inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500", href "/day1" ][ text "Day 1"]
-        ]
-        , li [ class "me-2"][
-            a [ class "inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300", href "/day2"][ text "Day 2"]
-        ]
-    ]
+
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
 
 main : Program () Model Msg
 main =
@@ -144,6 +205,7 @@ main =
         , onUrlRequest = LinkClicked
         , onUrlChange = UrlChanged
         }
+
 
 largeInput2015Day1 : String
 largeInput2015Day1 =
