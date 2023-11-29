f3dd522cf4c8b911c24fd11c0426e3e5eaf81f77
Jesse Warden <jesterxl@jessewarden.com>
2023-11-29 08:31:29 -0500

    images

--- copy.sh
+++ copy.sh
@@ -4,9 +4,9 @@ mkdir docs \
 && cp src/index.html docs/index.html \
 && cp src/tailwind.build.css docs/tailwind.build.css \
 && cp src/elm-hat.png docs/elm-hat.png \
-&& cp src/elm-hat.png docs/github-logo.png \
-&& cp src/elm-hat.png docs/jxl-helmet.jpg \
-&& cp src/elm-hat.png docs/jxl-logo.png \
-&& cp src/elm-hat.png docs/linkedin-logo.png \
-&& cp src/elm-hat.png docs/x-logo.png \
-&& cp src/elm-hat.png docs/youtube-logo.png
+&& cp src/github-logo.png docs/github-logo.png \
+&& cp src/jxl-helmet.jpg docs/jxl-helmet.jpg \
+&& cp src/jxl-logo.png docs/jxl-logo.png \
+&& cp src/linkedin-logo.png docs/linkedin-logo.png \
+&& cp src/x-logo.png docs/x-logo.png \
+&& cp src/youtube-logo.png docs/youtube-logo.png
