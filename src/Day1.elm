25a9e3df6548371cd5d6e3866ded8f7993a32053
Jesse Warden <jesterxl@jessewarden.com>
2023-12-01 07:28:39 -0500

    that took too long, lelz

--- src/Day1.elm
+++ src/Day1.elm
@@ -3,4 +3,16 @@ module Day1 exposing (getCalibration)
 
 getCalibration : String -> Int
 getCalibration puzzleInput =
-    142
+    String.split "\n" puzzleInput
+        |> List.map (\string -> String.filter Char.isDigit string)
+        |> List.map
+            (\str ->
+                if String.length str == 1 then
+                    String.append str str
+
+                else
+                    str
+            )
+        |> List.map (\str -> String.left 1 str ++ String.right 1 str)
+        |> List.filterMap String.toInt
+        |> List.sum
