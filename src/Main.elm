926f086061ab63ca06a7f62cea9dc33a7a04736b
Jesse Warden <jesterxl@jessewarden.com>
2023-12-01 11:31:35 -0500

    mispelling

--- src/Main.elm
+++ src/Main.elm
@@ -227,7 +227,7 @@ view model =
                                 , elmCode day1Part1CodeString
                                 ]
                             , div [ class "flex flex-col gap-6 block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" ]
-                                [ h5 [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" ] [ text "Ribbon Length" ]
+                                [ h5 [ class "mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" ] [ text "Trebuchet Enhanced Configs" ]
                                 , div [ class "font-normal text-gray-700 dark:text-gray-400" ] [ text ("Trebuchet Enhanced: " ++ formatInt model.day1.trebuchetConfigEnhanced) ]
                                 , button
                                     [ class "ext-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
