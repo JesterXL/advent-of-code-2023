1cc44b83d6325e90edd2c22b4f5af389d378cab2
Jesse Warden <jesterxl@jessewarden.com>
2023-12-02 10:22:03 -0500

    move2

--- src/Day2.elm
+++ src/Day2.elm
@@ -123,15 +123,6 @@ colorFromString colorString =
             Nothing
 
 
-sampleInput : String
-sampleInput =
-    """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
-
-
 parseAllGames : String -> List Game
 parseAllGames input =
     String.lines input
@@ -269,6 +260,15 @@ day2Part2CodeString =
 |> List.map powerOfCubeSet
 -- sum the products
 |> List.sum"""
+
+
+sampleInput : String
+sampleInput =
+    """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
 
 
 puzzleInput : String
