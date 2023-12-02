2db9691299dee88e35964282a9560bc40b0c9588
Jesse Warden <jesterxl@jessewarden.com>
2023-12-02 08:17:12 -0500

    parse gameID

--- src/Day2.elm
+++ src/Day2.elm
@@ -3,7 +3,25 @@ module Day2 exposing (parseGame)
 
 parseGame : String -> Game
 parseGame gameString =
-    Game 1 []
+    let
+        gameID =
+            String.split ":" gameString
+                |> List.take 1
+                |> List.map
+                    (\str ->
+                        String.split " " str
+                            |> List.drop 1
+                            |> List.head
+                            |> Maybe.withDefault "0"
+                    )
+                |> List.filterMap String.toInt
+                |> List.head
+                |> Maybe.withDefault 0
+
+        _ =
+            Debug.log "gameID" gameID
+    in
+    Game gameID [ { red = 0, blue = 0, green = 0 }, { red = 0, blue = 6, green = 0 } ]
 
 
 type alias Game =
--- tests/Day2Test.elm
+++ tests/Day2Test.elm
@@ -1,5 +1,6 @@
 module Day2Test exposing (..)
 
+import Array exposing (Array)
 import Day2 exposing (parseGame)
 import Dict
 import Expect exposing (Expectation)
@@ -11,12 +12,24 @@ suite : Test
 suite =
     describe "2023 - Day 2"
         [ describe "parseGame"
-            [ test "getCalibration should use puzzle example" <|
+            [ test "parseGame have an id of 1" <|
                 \_ ->
                     let
                         game =
                             parseGame "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
                     in
                     Expect.equal game.id 1
+            , test "parseGame should have a 2nd set of 6 blues" <|
+                \_ ->
+                    let
+                        game =
+                            parseGame "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+
+                        secondSet =
+                            Array.fromList game.sets
+                                |> Array.get 1
+                                |> Maybe.withDefault { red = 0, blue = 0, green = 0 }
+                    in
+                    Expect.equal secondSet.blue 6
             ]
         ]
